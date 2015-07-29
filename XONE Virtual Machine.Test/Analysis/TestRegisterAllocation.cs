52096ff0071dcae3727b05d1874e50e8c242abd5
Anton Jansson <anton_jansson@live.se>
2015-07-29 19:42:33 +0200

    Added register allocation using the linear-scan algorithm. The allocation has not been implemented in the code generator yet.

---
+++ XONE Virtual Machine.Test/Analysis/TestRegisterAllocation.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XONEVirtualMachine;
+using XONEVirtualMachine.Compiler.Analysis;
+using XONEVirtualMachine.Core;
+
+namespace XONE_Virtual_Machine.Test.Analysis
+{
+    /// <summary>
+    /// Tests the register allocation
+    /// </summary>
+    [TestClass]
+    public class TestRegisterAllocation
+    {
+        /// <summary>
+        /// Tests a simple function
+        /// </summary>
+        [TestMethod]
+        public void TestSimple()
+        {
+            using (var container = new Win64Container())
+            {
+                var intType = container.VirtualMachine.TypeProvider.GetPrimitiveType(PrimitiveTypes.Int);
+
+                var instructions = new List<Instruction>();
+                instructions.Add(new Instruction(OpCodes.LoadInt, 2));
+                instructions.Add(new Instruction(OpCodes.LoadInt, 4));
+                instructions.Add(new Instruction(OpCodes.AddInt));
+                instructions.Add(new Instruction(OpCodes.Ret));
+
+                var func = new Function(
+                    new FunctionDefinition("test", new List<VMType>() { }, intType),
+                    instructions,
+                    new List<VMType>());
+
+                container.VirtualMachine.Verifier.VerifiyFunction(func);
+
+                var virtualInstructions = VirtualRegisters.Create(func.Instructions);
+                var virtualControlFlowGraph = VirtualControlFlowGraph.FromBasicBlocks(
+                    VirtualBasicBlock.CreateBasicBlocks(new ReadOnlyCollection<VirtualRegisterInstruction>(virtualInstructions)));
+
+                var livenessIntervals = LivenessAnalysis.ComputeLiveness(virtualControlFlowGraph);
+                var registerAllocation = LinearScanRegisterAllocation.Allocate(livenessIntervals);
+
+                Assert.AreEqual(2, registerAllocation.Allocated.Count);
+                Assert.AreEqual(0, registerAllocation.Spilled.Count);
+            }
+        }
+
+        /// <summary>
+        /// Tests a simple function
+        /// </summary>
+        [TestMethod]
+        public void TestSimple2()
+        {
+            using (var container = new Win64Container())
+            {
+                var intType = container.VirtualMachine.TypeProvider.GetPrimitiveType(PrimitiveTypes.Int);
+
+                var instructions = new List<Instruction>();
+                instructions.Add(new Instruction(OpCodes.LoadInt, 2));
+                instructions.Add(new Instruction(OpCodes.LoadInt, 4));
+                instructions.Add(new Instruction(OpCodes.LoadInt, 6));
+                instructions.Add(new Instruction(OpCodes.AddInt));
+                instructions.Add(new Instruction(OpCodes.AddInt));
+                instructions.Add(new Instruction(OpCodes.Ret));
+
+                var func = new Function(
+                    new FunctionDefinition("test", new List<VMType>() { }, intType),
+                    instructions,
+                    new List<VMType>());
+
+                container.VirtualMachine.Verifier.VerifiyFunction(func);
+
+                var virtualInstructions = VirtualRegisters.Create(func.Instructions);
+                var virtualControlFlowGraph = VirtualControlFlowGraph.FromBasicBlocks(
+                    VirtualBasicBlock.CreateBasicBlocks(new ReadOnlyCollection<VirtualRegisterInstruction>(virtualInstructions)));
+
+                var livenessIntervals = LivenessAnalysis.ComputeLiveness(virtualControlFlowGraph);
+                var registerAllocation = LinearScanRegisterAllocation.Allocate(livenessIntervals, 2);
+
+                Assert.AreEqual(2, registerAllocation.Allocated.Count);
+                Assert.AreEqual(1, registerAllocation.Spilled.Count);
+            }
+        }
+
+        /// <summary>
+        /// Tests a simple function
+        /// </summary>
+        [TestMethod]
+        public void TestSimple3()
+        {
+            using (var container = new Win64Container())
+            {
+                var intType = container.VirtualMachine.TypeProvider.GetPrimitiveType(PrimitiveTypes.Int);
+
+                var instructions = new List<Instruction>();
+                instructions.Add(new Instruction(OpCodes.LoadInt, 1));
+                instructions.Add(new Instruction(OpCodes.LoadInt, 2));
+                instructions.Add(new Instruction(OpCodes.AddInt));
+                instructions.Add(new Instruction(OpCodes.LoadInt, 3));
+                instructions.Add(new Instruction(OpCodes.AddInt));
+                instructions.Add(new Instruction(OpCodes.LoadInt, 4));
+                instructions.Add(new Instruction(OpCodes.AddInt));
+                instructions.Add(new Instruction(OpCodes.LoadInt, 5));
+                instructions.Add(new Instruction(OpCodes.AddInt));
+                instructions.Add(new Instruction(OpCodes.Ret));
+
+                var func = new Function(
+                    new FunctionDefinition("test", new List<VMType>() { }, intType),
+                    instructions,
+                    new List<VMType>());
+
+                container.VirtualMachine.Verifier.VerifiyFunction(func);
+
+                var virtualInstructions = VirtualRegisters.Create(func.Instructions);
+                var virtualControlFlowGraph = VirtualControlFlowGraph.FromBasicBlocks(
+                    VirtualBasicBlock.CreateBasicBlocks(new ReadOnlyCollection<VirtualRegisterInstruction>(virtualInstructions)));
+
+                var livenessIntervals = LivenessAnalysis.ComputeLiveness(virtualControlFlowGraph);
+                var registerAllocation = LinearScanRegisterAllocation.Allocate(livenessIntervals);
+
+                Assert.AreEqual(2, registerAllocation.Allocated.Count);
+                Assert.AreEqual(0, registerAllocation.Spilled.Count);
+            }
+        }
+    }
+}
---
+++ XONE Virtual Machine/Compiler/Analysis/LinearScanRegisterAllocation.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XONEVirtualMachine.Compiler.Analysis
+{
+    /// <summary>
+    /// Represents a register allocation
+    /// </summary>
+    public class RegisterAllocation
+    {
+        /// <summary>
+        /// The allocated registers
+        /// </summary>
+        public IReadOnlyDictionary<LiveInterval, int> Allocated { get; }
+
+        /// <summary>
+        /// The spilled registers
+        /// </summary>
+        public IList<LiveInterval> Spilled { get; }
+
+        /// <summary>
+        /// Creates a new register allocation
+        /// </summary>
+        /// <param name="allocated">The allocated registers</param>
+        /// <param name="spilled">The spilled registers</param>
+        public RegisterAllocation(IDictionary<LiveInterval, int> allocated, IList<LiveInterval> spilled)
+        {
+            this.Allocated = new ReadOnlyDictionary<LiveInterval, int>(allocated);
+            this.Spilled = new ReadOnlyCollection<LiveInterval>(spilled);
+        }
+    }
+
+    /// <summary>
+    /// Represents register allocation using the linear-scan algorithm.
+    /// </summary>
+    public static class LinearScanRegisterAllocation
+    {
+        /// <summary>
+        /// Compares by end point
+        /// </summary>
+        private static int CompareByEndPoint(LiveInterval x, LiveInterval y)
+        {
+            return x.End.CompareTo(y.End);
+        }
+
+        /// <summary>
+        /// Allocates the registers
+        /// </summary>
+        /// <param name="liveIntervals">The live intervals</param>
+        /// <param name="numRegisters">The number of registers</param>
+        public static RegisterAllocation Allocate(IList<LiveInterval> liveIntervals, int numRegisters = 7)
+        {
+            var allocatedRegisteres = new Dictionary<LiveInterval, int>();
+            var spilledRegisters = new List<LiveInterval>();
+
+            var freeRegisters = new HashSet<int>(Enumerable.Range(0, numRegisters));
+            var active = new List<LiveInterval>();
+            liveIntervals = liveIntervals.OrderBy(x => x.Start).ToList();
+
+            foreach (var interval in liveIntervals)
+            {
+                ExplireOldIntervals(allocatedRegisteres, active, freeRegisters, interval);
+
+                if (active.Count == numRegisters)
+                {
+                    SplitAtInterval(allocatedRegisteres, spilledRegisters, active, interval);
+                }
+                else
+                {
+                    var freeReg = freeRegisters.First();
+                    freeRegisters.Remove(freeReg);
+                    allocatedRegisteres.Add(interval, freeReg);
+                    active.Add(interval);
+                    active.Sort(CompareByEndPoint);
+                }
+            }
+
+            return new RegisterAllocation(allocatedRegisteres, spilledRegisters);
+        }
+
+        /// <summary>
+        /// Expires old intervals
+        /// </summary>
+        /// <param name="allocatedRegisteres">The allocated registers</param>
+        /// <param name="active">The active registers</param>
+        /// <param name="freeRegisters">The free registers</param>
+        /// <param name="currentInterval">The current intervals</param>
+        private static void ExplireOldIntervals(
+            IDictionary<LiveInterval, int> allocatedRegisteres,
+            List<LiveInterval> active,
+            ISet<int> freeRegisters,
+            LiveInterval currentInterval)
+        {
+            foreach (var interval in active)
+            {
+                if (interval.End >= currentInterval.Start)
+                {
+                    return;
+                }
+
+                active.Remove(interval);
+                freeRegisters.Add(allocatedRegisteres[interval]);
+            }
+        }
+
+        /// <summary>
+        /// Splits at the given interval
+        /// </summary>
+        /// <param name="allocatedRegisteres">The allocated registers</param>
+        /// <param name="spilledRegisters">The spilled registers</param>
+        /// <param name="active">The active registers</param>
+        /// <param name="currentInterval">The current intervals</param>
+        private static void SplitAtInterval(
+            IDictionary<LiveInterval, int> allocatedRegisteres,
+            IList<LiveInterval> spilledRegisters,
+            List<LiveInterval> active,
+            LiveInterval currentInterval)
+        {
+            var spill = active.Last();
+
+            if (spill.End > currentInterval.End)
+            {
+                allocatedRegisteres.Add(currentInterval, allocatedRegisteres[spill]);
+
+                spilledRegisters.Add(spill);
+                allocatedRegisteres.Remove(spill);
+
+                active.Remove(spill);
+                active.Add(currentInterval);
+                active.Sort(CompareByEndPoint);
+            }
+            else
+            {
+                spilledRegisters.Add(currentInterval);
+                allocatedRegisteres.Remove(currentInterval);
+            }
+        }
+    }
+}
